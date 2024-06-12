17858d334da396f166829867cfafbe0d49655fdf
Saymon Leão <132489443+SaymonLion@users.noreply.github.com>
2024-06-11 22:16:46 -0300

    Add files via upload

--- cpasis/urls.py
+++ cpasis/urls.py
@@ -15,16 +15,56 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from cpasisApp.views import principal, new_eixo, editar_eixo, deletar_eixo, index, login, register
+from django.template.defaulttags import url
+from django.urls import path, re_path, include
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf import settings
+from cpasisApp.views import principal, new_eixo, editar_eixo, deletar_eixo, editar_dim,deletar_dim, new_dim
+from cpasisApp.views import new_ind, editar_ind, deletar_ind, new_campus, editar_campus, deletar_campus
+from cpasisApp.views import new_pub, deletar_publico, editar_publico, new_tipo, editar_tipo, deletar_tipo,editar_curso,new_curso,deletar_curso
+from cpasisApp.views import index, login, register, index1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('principal/', principal, name='principal'),
+    path('', principal, name='principal'),
     path('new_eixo/', new_eixo, name='new_eixo'),
     path('editar_eixo/<str:id>', editar_eixo, name='editar_eixo'),
     path('deletar/<str:id>',deletar_eixo, name='deletar_eixo'),
-    path('',index, name='index'),
-    path('login',login, name='login'),
-    path('register',register, name='register'),
+    # dimensão
+    path('new_dim/', new_dim, name='new_dim'),
+    path('editar_dim/<str:id>', editar_dim, name='editar_dim'),
+    path('deletar_dim/<str:id>',deletar_dim, name='deletar_dim'),
+    #indicadores
+    path('new_ind/', new_ind, name='new_ind'),
+    path('editar_ind/<str:id>', editar_ind, name='editar_ind'),
+    path('deletar_ind/<str:id>',deletar_ind, name='deletar_ind'),
+    # campus
+    path('new_campus/', new_campus, name='new_campus'),
+    path('editar_campus/<str:id>', editar_campus, name='editar_campus'),
+    path('deletar_campus/<str:id>',deletar_campus, name='deletar_campus'),
+    #publico
+    path('new_publico/', new_pub, name='new_pub'),
+    path('editar_publico/<str:id>', editar_publico, name='editar_publico'),
+    path('deletar_publico/<str:id>', deletar_publico, name='deletar_publico'),
+    #tipo
+    path('new_tipo/', new_tipo, name='new_tipo'),
+    path('editar_tipo/<str:id>', editar_tipo, name='editar_tipo'),
+    path('deletar_tipo/<str:id>', deletar_tipo, name='deletar_tipo'),
+    #curso
+    path('new_curso/', new_curso, name='new_curso'),
+    path('editar_curso/<str:id>', editar_curso, name='editar_curso'),
+    path('deletar_curso/<str:id>', deletar_curso, name='deletar_curso'),
+
+    path('index', index, name='index'),
+    path('index1', index1, name='index1'),
+    path('login', login, name='login'),
+    path('register', register, name='register'),
+
+    re_path(r'^img/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+
+
+
+
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
